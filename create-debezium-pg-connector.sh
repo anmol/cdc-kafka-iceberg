9c6b07f8a3ce66488a550cddec65fa80ec294e9f
anmol <anmolprakashgautam@gmail.com>
2024-08-12 07:13:01 +0800

    use pgoutput

--- create-debezium-pg-connector.sh
+++ create-debezium-pg-connector.sh
@@ -1,5 +1,7 @@
 # IP = $(hostname -I | cut -d ' ' -f 1)
 # change hostname accordingly
+# to delete: 
+# curl -X DELETE http://localhost:8083/connectors/cdc-using-debezium-connector
 curl --location 'http://localhost:8083/connectors' \
    --header 'Accept: application/json' \
    --header 'Content-Type: application/json' \
@@ -7,13 +9,14 @@ curl --location 'http://localhost:8083/connectors' \
    "name": "cdc-using-debezium-connector",
    "config": {
        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
-       "database.hostname": "192.168.1.11",
+       "database.hostname": "192.168.1.7",
        "database.port": "5443",
        "database.user": "postgres",
        "database.password": "123",
        "database.dbname": "cdc-using-debezium",
        "database.server.id": "184054",
        "table.include.list": "public.User",
-       "topic.prefix": "cdc-using-debezium-topic"
+       "topic.prefix": "cdc-using-debezium-topic",
+       "plugin.name": "pgoutput"
    }
 }'
--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+DROP TABLE "User";
 CREATE TABLE "User"
             (
                 "id"    uuid              NOT NULL DEFAULT uuid_generate_v4(),
@@ -8,6 +9,7 @@ CREATE TABLE "User"
                 CONSTRAINT "UQ_User_email" UNIQUE ("email"),
                 CONSTRAINT "PK_User_id" PRIMARY KEY ("id")
             );
+ALTER TABLE "User" REPLICA IDENTITY FULL;
 			
 INSERT INTO "User" (email, name)
 VALUES ('anmol+1@apg.com', CONCAT('name_', (random() * 1000)::INTEGER::VARCHAR))
@@ -16,3 +18,14 @@ RETURNING *;
 
 INSERT INTO "User" (email, name)
 VALUES ('anmol+2@apg.com', 'name_2');
+
+UPDATE "User"
+SET name = 'name_20'
+WHERE email = 'anmol+2@apg.com';
+
+DELETE
+FROM "User"
+WHERE email = 'anmol+2@apg.com';
+
+select * from "User";
+
